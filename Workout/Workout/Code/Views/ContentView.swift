a1b3024c0cd3fddda2f0dea05cfd1fd189ce9af6
Sarah <sarahireneclark@gmail.com>
2025-02-20 18:17:09 -0600

    Removed CoreData related code since it's not needed

---
+++ Workout/Workout/Code/Views/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Workout
+//
+//  Created by Sarah Clark on 2/19/25.
+//
+
+import SwiftUI
+import CoreData
+
+struct ContentView: View {
+
+    var body: some View {
+        NavigationStack {
+            Text("Hello, World!")
+        }
+    }
+
+}
+
+#Preview {
+    ContentView()
+}
--- Workout/Workout/Code/Views/WorkoutApp.swift
+++ Workout/Workout/Code/Views/WorkoutApp.swift
@@ -9,12 +9,10 @@ import SwiftUI
 
 @main
 struct WorkoutApp: App {
-    let persistenceController = PersistenceController.shared
 
     var body: some Scene {
         WindowGroup {
             ContentView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
 }
